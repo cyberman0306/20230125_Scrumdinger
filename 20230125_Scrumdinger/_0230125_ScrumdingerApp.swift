d708e2d2ae21ee52ffb083779b5d24661d317c15
HYUN JUN YOOK <yhj03069@gmail.com>
2023-01-25 15:34:41 +0900

    Initial Commit

---
+++ 20230125_Scrumdinger/_0230125_ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0230125_ScrumdingerApp.swift
+//  20230125_Scrumdinger
+//
+//  Created by cnu on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct _0230125_ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
